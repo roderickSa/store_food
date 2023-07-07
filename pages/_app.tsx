69e5df0e63de37ca95bc4d45c896534608ca19c6
roderickSanchez <roderick.sanchez@justo.mx>
2023-07-07 09:59:33 -0500

    fix import styles components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "#/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import type { AppProps } from "next/app";
