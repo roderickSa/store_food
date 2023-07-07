a14601cd9bb01a8dcec3248f4ae74ffdec4c2067
roderickSanchez <roderick.sanchez@justo.mx>
2023-07-07 09:56:05 -0500

    fix import index components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Footer from "@/src/components/Footer/Footer";
-import Header from "@/src/components/Header/Header";
-import Home from "@/src/components/Home/Home";
-import Layout from "@/src/components/Layout/Layout";
-import Loader from "@/src/components/Loader/Loader";
+import Footer from "@/components/Footer/Footer";
+import Header from "@/components/Header/Header";
+import Home from "@/components/Home/Home";
+import Layout from "@/components/Layout/Layout";
+import Loader from "@/components/Loader/Loader";
 
 export default function Index() {
   return (
